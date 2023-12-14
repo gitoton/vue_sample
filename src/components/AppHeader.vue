d57532920b81fff0c562c381501cdcfafaacdc2e
gitoton <pgmamire@gmail.com>
2023-12-14 21:19:22 +0900

    done some extent

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,4 +13,19 @@
 </script>
 
 <style scoped>
+    header{
+        border-bottom: 2px solid #0e271c;
+    }
+    .container h1{
+        position: relative;
+        text-align: center;
+        height: 50px;
+        line-height: 50px;
+    }
+    .container h1 img{
+        display: inherit;
+        text-align: center;
+        margin: auto;
+        height: 50px;
+    }
 </style>
